aa86e276b952dfb2d75a34da2be8c31c88efcb83
Naveed Jamali <naveedjamali@outlook.com>
2022-07-14 15:48:01 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/effordea/flutter_animations/flutter_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.effordea.flutter_animations.flutter_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(AnimationApp());
+}
+
+class AnimationApp extends StatefulWidget {
+  const AnimationApp({Key? key}) : super(key: key);
+
+  @override
+  State<AnimationApp> createState() => _AnimationAppState();
+}
+
+class _AnimationAppState extends State<AnimationApp> {
+  double height = 500;
+  double width = 500;
+
+  bool callingFirstTime = true;
+
+  @override
+  Widget build(BuildContext context) {
+    if (callingFirstTime) {
+      Future.delayed(const Duration(seconds: 2), () => setState(setDimension));
+      callingFirstTime = false;
+    }
+    return MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: AnimatedContainer(
+            curve: Curves.easeInOut,
+            duration: const Duration(seconds: 1),
+            // color: Colors.green,
+            width: height,
+            height: width,
+            child: Transform(
+                transform: Matrix4.skew(-1, 50),
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                  size: height,
+                ))),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        setState(setDimension);
+      }),
+    ));
+  }
+
+  setDimension() {
+    if (height == 500) {
+      height = width = 200;
+    } else {
+      height = width = 500;
+    }
+
+    Future.delayed(const Duration(seconds: 2), () => setState(setDimension));
+  }
+}
